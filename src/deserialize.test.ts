be9e36f824e3056e0a3a6345fb6cb09800f52f26
David Iunevich <f3oall@gmail.com>
2024-10-26 03:46:56 +0400

    improved test cases

--- src/deserialize.test.ts
+++ src/deserialize.test.ts
@@ -10,29 +10,44 @@ enum ExampleEnum {
 interface ExampleData {
 	id: string;
 	age: number;
+	date?: Date;
 	enum?: ExampleEnum;
 }
 
 // just to make sure the wrapper works
 it("should work with wrapper", () => {
-	const json = `{"id": "1", "age": 10, "enum": "a"}`;
+	const json = `{"id": "1", "age": 10, "enum": "a", "date": "2021-01-01T00:00:00.000Z"}`;
 	const data = deserialize<ExampleData>(json);
 	expect(data.id).toBe("1");
 	expect(data.age).toBe(10);
+	expect(data.enum).toBe(ExampleEnum.A);
+	expect(data.date).toEqual(new Date("2021-01-01T00:00:00.000Z"));
 });
 
 // this one should throw but it doesn't
-it("should throw with wrapper", () => {
+it("should throw when properties missing (with wrapper)", () => {
 	const json = `{"id": "1"}`;
 	expect(() => deserialize<ExampleData>(json)).toThrow();
 });
 
 // this one isn't throwing either
 // //however, if we don't use the wrapper, it throws as expected
+it("should throw when properties missing", () => {
+	const json = `{"enum": "A"}`;
+	const data: unknown = JSON.parse(json);
+	expect(() => {
+		deepkitDeserialize<ExampleData>(data, { loosely: false });
+	}).toThrow();
+});
+
+// this one isn't throwing either
+// //however, if we don't use the wrapper, it throws as expected
 it("should throw", () => {
-	const json = `{"id": "1"}`;
-	const data = JSON.parse(json);
-	expect(() => deepkitDeserialize<ExampleData>(data)).toThrow();
+	const json = `{"enum": "A", "id": "1", "age": "23"}`;
+	const data: unknown = JSON.parse(json);
+	expect(() => {
+		deepkitDeserialize<ExampleData>(data, { loosely: false });
+	}).toThrow();
 });
 
 // also, this one throws as expected, so it might have something to do with property optionality guards?
--- src/deserialize.ts
+++ src/deserialize.ts
@@ -1,7 +1,13 @@
-import { deserialize as deepkitDeserialize, type ReceiveType } from "@deepkit/type";
+import { deserialize as deepkitDeserialize, ReceiveType } from "@deepkit/type";
 
 export function deserialize<T>(json: string, type?: ReceiveType<T>) {
-    const data = JSON.parse(json);
+	const data = JSON.parse(json);
 
-    return deepkitDeserialize<T>(data, undefined, undefined, undefined, type)
+	return deepkitDeserialize<T>(
+		data,
+		{ loosely: false },
+		undefined,
+		undefined,
+		type,
+	);
 }
